c718319d61e8b1cd05a428ac6b948bc93476fa49
Akshat Naik <akshatn77@gmail.com>
2020-04-29 21:38:59 +0300

    Made code clearer and less stringent.

--- tic_main.py
+++ tic_main.py
@@ -22,16 +22,18 @@ class Board:
             True if placement successful, else False.
         
         piece: string taking one of two values X or O.
-        location: string in the form of (x,y).
+        location: string in the form of x,y.
         where x is the row and y is the column number.
         """
-        x, y = int(location[1]) - 1, int(location[3]) - 1
+        loc = location.split(",")
+        x,y = int(loc[0]), int(loc[1])
         try:
             if self.grid[x][y] == " ":
                 self.grid[x][y] = piece
                 return True
         except IndexError:
             return False
+        return False
 
     # Could be improved later by checking for only one player.
     def winning(self, piece): 
@@ -42,11 +44,7 @@ class Board:
         piece: string taking one of two values X or O.
         """
         rows = [self.grid[0], self.grid[1], self.grid[2]]
-        cols = [
-            [self.grid[i][0] for i in range(3)],
-            [self.grid[i][1] for i in range(3)],
-            [self.grid[i][2] for i in range(3)]
-        ]
+        cols = list(zip(*self.grid))
         diag = [
             [self.grid[i][i] for i in range(3)],
             [self.grid[i][2-i] for i in range(3)]
@@ -84,8 +82,8 @@ class Agent:
         board: the current instance of board class.
         """
         print("Player " + self.piece + ":")
-        error = True
-        while error:
-            location = input("Enter location of placement in the form of (x,y): ")
-            error = not board.change_state(self.piece, location)
+        failure = True
+        while failure:
+            location = input("Enter location of placement in the form of x,y: ")
+            failure = not board.change_state(self.piece, location)
 
